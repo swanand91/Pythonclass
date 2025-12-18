b233a192255ccc8d013966295220011092907ae4
swanand91 <swanandjoshi871@gmail.com>
2025-12-18 15:54:30 +0530

    assignment on python initial commit

---
+++ Task1.py
@@ -0,0 +1,13 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+
+result = first_number + second_number
+print("Addition:",result)
+result2 = first_number - second_number
+print("Subtraction:",result2)
+
+result = first_number * second_number
+print("Multiplication:",result)
+result = first_number / second_number
+print("Division:",result)
